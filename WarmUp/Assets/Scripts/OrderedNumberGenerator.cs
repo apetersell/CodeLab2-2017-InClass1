c24627fc3ac34f4840acb7386cc45519f86a7fca
Andrew Petersell <andrewpetersell@gmail.com>
2017-09-08 15:15:55 -0400

    Inherited Number Generators

---
+++ WarmUp/Assets/Scripts/OrderedNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderedNumberGenerator : NumberGenerator {
+
+	public int[] order;
+
+	int current;
+
+	public override int Next ()
+	{
+		int result = order [current % order.Length];
+
+		current++;
+
+		return result;
+	}
+}
---
+++ WarmUp/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public Sprite [] sprites;
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating ("spriteTime", 1, 1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void spriteTime () {
+		int nextNum = GetComponent<NumberGenerator> ().Next();
+
+		makeSprite (nextNum);
+
+	}
+
+	void makeSprite (int num) {
+		GameObject goSprite = new GameObject ();
+		SpriteRenderer sr = goSprite.AddComponent<SpriteRenderer> ();
+		sr.sprite = sprites[num];
+		goSprite.AddComponent<Rigidbody2D> ();
+
+		
+	}
+}
